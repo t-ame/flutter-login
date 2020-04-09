096770c55a9de9ff4b99c539ed831806860b644c
Toya Amechi <toya.amechi@gmail.com>
2020-04-09 03:30:34 +0100

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MyApp()
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,53 +18,186 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         accentColor: Colors.amberAccent
       ),
-      home: MyHomePage(),
-//      home: MyHomePage(title: 'Login Page'),
+      home: AnotherPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class AnotherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.all(75)),
+                  MyImage(),
+                  Padding(padding: EdgeInsets.all(20)),
+                  MyAppName(),
+                  Padding(padding: EdgeInsets.all(20)),
+                  SignUpPageButton(),
+                  Padding(padding: EdgeInsets.all(20)),
+                ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class SignUpPageButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text(
+          'Sign up',
+          style: TextStyle(fontSize: 25)
+    ),
+      color: Color(0xFF42A5F5),
+      textColor: Colors.amberAccent[700],
+      onPressed: () {},
+    );
+  }
+}
+
+class MyAppName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text.rich(
+      TextSpan(
+        style: TextStyle(
+          fontSize: 40,
+          fontFamily: "Calibri, Arial, Tahoma, Serif",
+        color: Color(0xFF42A5F5)
+        ),
+        children: <TextSpan>[
+          TextSpan(text: 'Toy\'s ', style: TextStyle(fontStyle: FontStyle.italic)),
+          TextSpan(text: 'DICE', style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
+
+class MyImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image(
+        image: NetworkImage("https://www.iconsdb.com/icons/preview/orange/dice-xxl.png"),
+        height: 180,
+        width: 180,
+      ),
+    );
+  }
+}
+
+
+
+
+//            Container(
+//              decoration: BoxDecoration(
+////            color: const Color(0xff7c94b6),
+//                  color: Colors.white54,
+//                  border: Border.all(
+//                    color: const Color(0xff7c94b6),
+//                    width: 0.6,
+//                  ),
+//                  borderRadius: BorderRadius.circular(10),
+//                  boxShadow: [
+//                    BoxShadow(
+//                        blurRadius: 5,
+//                        color: Colors.grey,
+//                        offset: Offset.fromDirection(1, 4),
+//                        spreadRadius: 2
+//                    )
+//                  ]
+//              ),
+//              height: 300,
+//              width: 250,
+//              child: Column(
+//                children: <Widget>[
+//                  TextField(
+//                  ),
+//                  TextField(
+//                  ),
+//                  Text("Some Text",
+//                    style: TextStyle(
+//                        fontSize: 30,
+//                        color: Colors.teal,
+//                        fontWeight: FontWeight.w400
+//                    ),
+//                  ),
+//                  SignUpPageButton(),
+//                  GestureDetector(
+//                    onTap: () {
+//                      print('MyButton was tapped!');
+//                    },
+//                    child: Container(
+//                      height: 36.0,
+//                      padding: const EdgeInsets.all(8.0),
+//                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+//                      decoration: BoxDecoration(
+//                        borderRadius: BorderRadius.circular(5.0),
+//                        color: Colors.lightGreen[500],
+//                      ),
+//                      child: Center(
+//                        child: Text('Engage'),
+//                      ),
+//                    ),
+//                  )
+//                ],
+//              ),
+//            ),
+
+
+
+
+
+
+//
+//class MyLoginPage extends StatefulWidget {
+////  MyHomePage({Key key, this.title}) : super(key: key);
+////  final String title;
+//  @override
+//  _MyLoginPageState createState() => _MyLoginPageState();
+//}
+//
+//class _MyLoginPageState extends State<MyLoginPage> {
+//  int _counter = 0;
+//
+//  void _incrementCounter() {
+//    setState(() {
+//      _counter++;
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+////      appBar: AppBar(
+////        title: Text(widget.title),
+////      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text(
+//              'You have pushed the button this many times:',
+//            ),
+//            Text(
+//              '$_counter',
+//              style: Theme.of(context).textTheme.headline4,
+//            ),
+//          ],
+//        ),
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _incrementCounter,
+//        tooltip: 'Increment',
+//        child: Icon(Icons.add),
+//      ), // This trailing comma makes auto-formatting nicer for build methods.
+//    );
+//  }
+//}
+
+
